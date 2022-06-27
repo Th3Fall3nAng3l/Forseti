b0b38efa064aabc67425c55adecee676a174b367
Th3Fall3nAng3l <98032018+Th3Fall3nAng3l@users.noreply.github.com>
2022-06-27 14:37:04 +0200

    Add files via upload

--- Forseti.sh
+++ Forseti.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
-clear
+
+#~~~~~~~~~~~~~~~~~~~~#
+#                    #
+# Forseti v1.0       #
+# Distribution Linux #
+# Auteur : Expinfo   #
+#                    #
+#~~~~~~~~~~~~~~~~~~~~#
 
 #~~~~~Variables modifiables~~~~~
 
 User="freebox" #Nom d'utilisateur pour la connexion au SFTP
+
 Passwd="7VF9hf64n!N79" #Mdp pour la connexion au SFTP
+
 Server="cyber-lab.hd.free.fr"
+
 ServerPort="22731"
+
 ServerRep="FTP_EXPINFO/Forseti_DFIR/"
-NetMask="192.168.1.0/24" #Masque du réseau
+
+NetMask="192.168.1.0/24" #Plage d'adresse ip a scanner
+
+SuUsername="root"
+
 SuPasswd="JeSuis1Mot2Passe"
 
 #~~~~~Variables statiques~~~~~
 
 RepPC=$PWD
-NOW=$( date '+%F' )
 
 #~~~~~Initialisation~~~~~
 
+clear
 if [ $UID != "0" ]
 then
   echo "Vous n'avez pas les droits requis pour l'execution du programme..."
@@ -60,9 +75,9 @@ apt-get -qq -y install curl
 apt-get -qq -y install nmap
 mkdir $RepPC/Temp_Expinfo_CyLR/Tools/CyLR
 cd $RepPC/Temp_Expinfo_CyLR/Tools/CyLR
-wget -q https://github.com/orlikoski/CyLR/releases/download/2.2.0/CyLR_linux-x64.zip
-unzip CyLR_linux-x64.zip > /dev/null
-rm -rf CyLR_linux-x64.zip
+curl -s --ftp-ssl --insecure ftp://$Server:$ServerPort/FTP_EXPINFO/CyLR/CyLRLinux.zip -u $User:$Passwd --output CyLRLinux.zip
+unzip CyLRLinux.zip > /dev/null
+rm -rf CyLRLinux.zip
 
 kill -9 $SPIN_ID > /dev/null
 printf '[\342\234\224] Fait.\n' | iconv -f UTF-8
@@ -89,6 +104,8 @@ disown
 
 nmap -n -sn $NetMask -oG - | awk '/Up$/{print $2}' > Liste_Adresse.txt #Récupération des adresses sur le réseau pour le lancement en parallèle de la récup de ressources
 mv Liste_Adresse.txt $RepPC/Temp_Expinfo_CyLR/Temps
+cd $RepPC/Temp_Expinfo_CyLR/Temps
+sed -i 's/^/'$SuUsername'@/' Liste_Adresse.txt
 
 kill -9 $SPIN_ID > /dev/null
 printf '[\342\234\224] Fait.\n' | iconv -f UTF-8
@@ -114,11 +131,14 @@ SPIN_ID=$!
 disown
 
 cd $RepPC/Temp_Expinfo_CyLR/Temps
-sshpass -p $SuPasswd parallel-ssh -h Liste_Adresse.txt -A -l root 'apt-get -qq -y install git; mkdir /Temp_Expinfo_CyLR; cd /Temp_Expinfo_CyLR; git clone https://github.com/Th3Fall3nAng3l/Forseti_pssh; cd Forseti_pssh;chmod 755 ./Forseti_pssh.sh ;./Forseti_pssh.sh'
+while IFS= read -r line; do
+    sshpass -p $SuPasswd ssh -f $line apt-get -qq -y install git; mkdir /Temp_Expinfo_CyLR; cd /Temp_Expinfo_CyLR; git clone https://github.com/Th3Fall3nAng3l/Forseti_pssh; cd Forseti_pssh;chmod 755 ./Forseti_pssh.sh ;./Forseti_pssh.sh
+done < $RepPC/Temp_Expinfo_CyLR/Temps/Liste_Adresse.txt
+#sshpass -p $SuPasswd parallel-ssh -h Liste_Adresse.txt -A -l $SuUsername 'apt-get -qq -y install git; mkdir /Temp_Expinfo_CyLR; cd /Temp_Expinfo_CyLR; git clone https://github.com/Th3Fall3nAng3l/Forseti_pssh; cd Forseti_pssh;chmod 755 ./Forseti_pssh.sh ;./Forseti_pssh.sh'
 
 cd $RepPC/Temp_Expinfo_CyLR/Tools/CyLR
-./CyLR -q -of "$NOW"_"$HOSTNAME"_CyLR.zip >/dev/null #Lancement de l'outil CyLR
-curl -s --ftp-ssl --insecure -T "$NOW"_"$HOSTNAME"_CyLR.zip ftp://$Server:$ServerPort/$ServerRep/ --user $User:$Passwd #Transfert du fichier .zip obtenu sur le serveur ftp avec un protocole ssl
+./CyLR -q -of "$HOSTNAME"_CyLR.zip >/dev/null #Lancement de l'outil CyLR
+curl -s --ftp-ssl --insecure -T "$HOSTNAME"_CyLR.zip ftp://$Server:$ServerPort/$ServerRep/ --user $User:$Passwd #Transfert du fichier .zip obtenu sur le serveur ftp avec un protocole ssl
 printf '[\342\234\224] Fait.\n' | iconv -f UTF-8
 
 kill -9 $SPIN_ID > /dev/null
